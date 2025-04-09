cef126c150dcb812b9cf91966e279e3acf5ea78b
Romel Rey Silva <sromelrey@gmail.com>
2025-04-09 20:22:54 +0800

    feat(params, query and body): nest js @Param, @Query and @Body decorator basic uses + httpyac sample GET params and query and POST body extraction

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Param, Query, Body } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  // ?  @Get('{/:id}') Optional parameter
+  @Get('/:id') // ? Required Parameter
+  public getUsers(@Param() params: any, @Query() query: any) {
+    console.log(params, query);
+    return 'You sent a get request to users endpoint';
+  }
+
+  @Post()
+  public createUsers(@Body() request: any) {
+    console.log(request);
+    return 'You sent a post request to users endpoint';
+  }
+}
